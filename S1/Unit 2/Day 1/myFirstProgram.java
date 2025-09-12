fa13c50ed15f48e5ac18bc7469121077c7d28628
Henry Veedahl <henry.veedahl@outlook.com>
2025-09-12 14:18:50 -0500

    Day 1

---
+++ S1/Unit 2/Day 1/myFirstProgram.java
@@ -0,0 +1,45 @@
+/*
+Henry Veedhal
+This Program Prints A message to the screen
+It also shows the basic structure of a program
+Comments Don't Run As Code
+The Compyter Dosen't Understand Comments or Look at them
+Every Program Should Have Comments to help you undertand
+// indicate a comment for a single line
+
+We Will Write These Comments Today
+
+Forward Slash star indicates the start if block/multiline comments
+*/
+
+//Every Program Will Have the class decloration
+
+public class myFirstProgram //myFirstProgram is the name of the program and will be my file name
+{//This Bracket Starts My Program
+    //public static void main(String[] args) { //I need this every program. This starts my Main
+        //Inside of main is bettwen the {}
+        //System.out.print("Hello World"); //All commands have ;
+        //System.out.print will print on the same lin
+        //Does not move to new line
+        //System.out.print("Today is wendsday");
+        // println goes to next line at the end
+        //System.out.println("This is a print");
+        //System.out.println("Its almost the end of the day");
+        //System.out.print("A Backslash looks like this \\ \nthis is kind of cool");
+        // \ tells java we want to print a symbol
+        // We usally use \ to use a symbol java already uses like "" or a /
+        // \\ prints \
+        // \n prints a new line
+
+    //}//end of main
+    //
+    public static void main(String[] args) {
+        System.out.println("^_____^");
+        System.out.println("<     >");
+        System.out.println("(\"^ ^\")");
+        System.out.println(" >=0=< ");
+        System.out.println("   V  ");
+    }
+}
+//This Bracket Ends My Program
+// If you have a start brakcket
