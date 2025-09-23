626653a1b759bb5e562be07ecbc2a9b9b059f398
Henry Veedahl <henry.veedahl@outlook.com>
2025-09-23 15:09:14 -0500

    Unit 2 Lab

---
+++ S1/Unit 2/Lab/buggyCodeErrorsLab.java
@@ -0,0 +1,23 @@
+public class buggyCodeErrorsLab { //Start of Program
+
+    public static void main(String[] args) {//Main Star
+        //Setup Varibles
+        String firstName = "Bob";
+        String lastName = "Steve";
+        Double age = 7.5;
+        double GPA = 3.9;
+        String favoriteClass = "Computer Science";
+
+        //Print Infromation
+        System.out.println("*******************************************");
+        System.out.println("*                                         *");
+        System.out.println("*            Student Information          *");
+        System.out.println("*                                         *");
+        System.out.println("*******************************************");
+        System.out.println("*        Student Name: "+firstName+" "+lastName+"          *");
+        System.out.println("*           Sudent Age: "+age+"               *");
+        System.out.println("*           Current GPA: "+GPA+"              *");
+        System.out.println("*     Favorite Class: "+favoriteClass+"    *");      
+        System.out.println("*******************************************");
+    }//End of main
+} //End of program
