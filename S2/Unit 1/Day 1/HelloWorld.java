b4e5d8169be55fd17fc9d9d12fd43b0972de4c80
Henry Veedahl <89225651+HenryNerd@users.noreply.github.com>
2026-01-26 17:54:56 -0600

    S2 Unit 1 Day 1

---
+++ S2/Unit 1/Day 1/HelloWorld.java
@@ -0,0 +1,7 @@
+//Henry Veedahl
+//This is the second semester of AP CSA
+public class HelloWorld{ //Start Class
+    public static void main(String[] uriel){ //Start Main
+        System.out.println("Hello World");
+    } //End Main
+} //End Class
